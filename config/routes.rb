ac19f708d382d48d66a2b26ed01e5c4a4218c145
Adam Freemer <a@freemer.com>
2019-09-24 19:51:40 -0400

    Updated routes file to include relevant link and top url resources and actions

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :top_urls, only: [:index]
-      resource :links, only: [:create, :show]
+      resources :links, only: [:create, :show]
     end
   end
 end
