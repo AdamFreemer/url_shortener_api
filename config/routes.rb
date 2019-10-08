f3f0fff9c290a6d5e84eeaee65de061d14a962b0
Adam Freemer <a@freemer.com>
2019-10-07 23:38:53 -0400

    Refactor link show route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :top_urls, only: [:index]
-      resources :links, only: [:create, :show]
+      resources :links, only: [:create]
+      
     end
   end
+  get '/:id', to: 'api/v1/links#show'
 end
