dccfe577d13c27bd5e1a76e9ef985482038239e3
Adam Freemer <a@freemer.com>
2019-10-17 21:25:26 -0400

    Update busy api to reflect jobs processed que

--- app/controllers/api/v1/busy_controller.rb
+++ app/controllers/api/v1/busy_controller.rb
@@ -2,11 +2,10 @@ module Api::V1
     class BusyController < ApplicationController
       include Utility
       require 'sidekiq/api'
-      stats = Sidekiq::Stats.new
 
       def index
         stats = Sidekiq::Stats.new
-        json_response(stats.processed)
+        json_response(stats.enqueued)
       end
     end
   end
